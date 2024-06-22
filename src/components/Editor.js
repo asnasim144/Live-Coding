ec39bba409bc59f2aeab4f5e1a97e36785783188
asnasim144 <93252806+asnasim144@users.noreply.github.com>
2024-06-22 06:49:13 +0600

    sdf

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,7 +5,7 @@ export default function Editor({ language,codes, setCodes }) {
   return (
     <div className=""> 
       <div className="">
-        <div className=""><b>{language} Code Editor</b></div>
+        <div className=""><b>{language} Editor</b></div>
       </div>
       <div className="">
         <textarea  className={`w-full h-24 border-2  ${language === "HTML" ? 'border-orange-600' : language === "CSS" ? 'border-sky-500' : language === "JS" ? 'border-yellow-500' : language === "Meta" ? 'border-gray-500' :  ""}`} onChange={handleCodeChange}>{codes}</textarea>
